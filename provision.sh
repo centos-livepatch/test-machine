8dce2f61a97effb8ff1876f121500dba508c63fa
Louis Taylor <louis@kragniz.eu>
2015-05-10 18:32:50 +0100

    Add the rest of the dependencies

--- provision.sh
+++ provision.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
+# kpatch dependencies
 sudo yum install gcc kernel-devel elfutils elfutils-devel
+
+# kpatch-build dependencies
+sudo yum install \
+    rpmdevtools \
+    pesign \
+    yum-utils \
+    zlib-devel \
+    binutils-devel \
+    newt-devel \
+    python-devel \
+    perl-ExtUtils-Embed \
+    audit-libs \
+    audit-libs-devel \
+    numactl-devel \
+    pciutils-devel \
+    bison
+
+sudo yum-config-manager --enable debug
+sudo yum-builddep kernel
+sudo debuginfo-install kernel
+
+# cache compilation
+sudo yum install ccache
+ccache --max-size=5G
